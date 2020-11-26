6c4233b2cfed449378bd9cc1cd778505075d4a0c
Creniaut Benjamin <creniaut.benjamin@gmail.com>
2020-11-26 15:18:20 +0100

    premiere version de l'index, reste a faire tags

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ fetch('jsonSource.json')
         return response.json()
     })
 
+    
+
     .then ((data) => {
+      
+        var model = document.querySelector("#card");
 
-        var testa = document.querySelector("#idp")
-
-        testa.innerHTML = data["photographers"][0]["name"]
-
-
-
-    }
- )
+        data["photographers"].forEach(element => {
+            
+            document.querySelector("#card a h2").innerHTML = element["name"];
+            document.querySelector("#card a div img").src = "Photos/PhotographersPhotos/" + element['portrait'];
+            document.querySelector("#card h3").innerHTML = element["country"] +", " + element["city"];
+            document.querySelector("#card p").innerHTML = element["tagline"];
+            document.querySelector("#card small").innerHTML = element["price"] + "€/Jour";
+                       
+            var clone = model.cloneNode(true);
+                        
+            clone.id = element["id"];
+                       
+            document.querySelector("main").appendChild(clone);
+                
+        }
+    )})
 
 
 
